788cd37c5f628135a84ea51ce93b25ad6790bebb
Matthew Rumschlag <mjrumschlag@gmail.com>
2025-09-23 14:59:51 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,8 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Return Request Demo' });
 });
 
-router.post('/addviewer', (req, res) => {
-	res.io.to(req.params.id).emit('addviewer', { name: req.params.name});
-});
-
-router.post('/removeviewer', (req, res) => {
-	res.io.to(req.params.id).emit('removeviewer', { name: req.params.name });
+router.post('/', (req, res) => {
+	res.io.to(req.params.id).emit('viewerschanged', { id: req.params.id});
 });
 
 module.exports = router;
