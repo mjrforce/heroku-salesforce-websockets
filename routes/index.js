6f067b24b723cdd8b483fd9a842959e67ed82816
Matthew Rumschlag <mjrumschlag@gmail.com>
2025-09-23 17:52:02 -0400

    stuff

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var bodyparser = require('body-parser');
+var jsonparser = bodyparser.json();
 
 
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Return Request Demo' });
 });
 
-router.post('/', (req, res) => {
+router.post('/', jsonparser, (req, res) => {
 	console.log('request params: ', JSON.stringify(req.body));
 	res.io.to(req.body.id).emit('viewerschanged', { id: req.body.id});
 	res.send({status: 'ok'});
