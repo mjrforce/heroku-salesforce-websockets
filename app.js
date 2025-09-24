1b8b6ea6aca9f64a7bc073c711d5b4a9da4b12f0
Matthew Rumschlag <mjrumschlag@gmail.com>
2025-09-23 21:10:01 -0400

    stuff

--- app.js
+++ app.js
@@ -25,11 +25,18 @@ var io = require('socket.io')(server, {
 
 //On Connection Event
 var socket = io.sockets.on('connection', function (socket) {
-    
-    console.log('Joining Room: ' + socket.handshake.auth.recordId);
-    let payload = { id: socket.handshake.recordId};
-    socket.to(socket.handshake.auth.recordId).emit('viewerschanged', payload)
-    socket.join(socket.handshake.auth.recordId);
+
+    let recordId = socket.handshake.auth.recordId;
+    let username = socket.handshake.auth.name;
+    console.log('Joining Room: ' + recordId);
+    let payload = { id: recordId, username: username };
+
+    socket.to(recordId).emit('viewerconnected', payload)
+    socket.join(recordId);
+
+    socket.on('disconnecting', function(socket){
+      socket.to(recordId).emit('viewerdisconnected', payload);
+    });
  });
 
 // setup view engine 
